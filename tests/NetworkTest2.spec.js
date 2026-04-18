2371825ba4a5c1e041ad5a20796beb3b034309a9
sharmadotexe <rockvikas300@gmail.com>
2026-04-18 21:05:11 +0530

    security spec file

---
+++ tests/NetworkTest2.spec.js
@@ -0,0 +1,18 @@
+const { request, expect, test } = require('@playwright/test')
+
+//Lecture Number 68
+test('Security Test', async ({ page }) => {
+
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+    await page.locator("#userEmail").fill("Usertest1211@gmail.com");
+    await page.locator("#userPassword").fill("Password@1");
+    await page.locator("#login").click();
+    await page.locator(".card-body b").first().waitFor();
+
+    await page.locator("button[routerlink='/dashboard/myorders']").click();
+
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*",
+        route => route.continue({ url: "https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=69e35fe0f86ba51a6570961e" }));
+    await page.locator("button:has-text('View')").first().click();
+    await page.pause();
+})
